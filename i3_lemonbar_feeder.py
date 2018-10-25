7878217d82fd94e30393da0e756e586a8a0d8518
jeff cochran <mercurythewhite@gmail.com>
2018-10-25 14:13:38 -0700

    added cpu load

--- i3_lemonbar_feeder.py
+++ i3_lemonbar_feeder.py
@@ -9,92 +9,112 @@ from threading import Thread
 from subprocess import Popen, PIPE, check_output, TimeoutExpired
 from i3_lemonbar_conf import *
 
+
 class LemonBar(object):
 
-	def __init__(self, i3):
-		self.i3              = i3
-		self.focusedWinTitle = self.i3.get_tree().find_focused().name
+    def __init__(self, i3):
+        self.i3 = i3
+        self.focusedWinTitle = self.i3.get_tree().find_focused().name
 
-	def on_window_title_change(self, caller, e):
-		self.focusedWinTitle = e.container.name
-		self.render()
+    def on_window_title_change(self, caller, e):
+        self.focusedWinTitle = e.container.name
+        self.render()
 
-	def render_workspaces(self, display):
-		wsp_icon = "%%{F%s B%s} %%{T2}%s%%{T1}" % (color_back, color_head, icon_wsp)
-		wsp_items = ''
-		for wsp in self.i3.get_workspaces():
-			wsp_name = wsp['name']
-			wsp_action = ""
-			if wsp['output'] != display and not wsp['urgent']:
-				continue
-			if wsp['focused']:
-				# Possibly add dark red tint if urgent
-				wsp_items += "%%{F%s B%s}%s%s%%{F%s B%s T1} %s %%{F%s B%s}%s" % (color_head,
-					color_wsp, sep_right, wsp_action, color_back, color_wsp, wsp_name,
-					color_wsp, color_head, sep_right)
-			elif wsp['urgent']:
-				wsp_items += "%%{F%s B%s}%s%s%%{F%s B%s T1} %s %%{F%s B%s}%s" % (color_head,
-					color_mail, sep_right, wsp_action, color_back, color_mail, wsp_name,
-					color_mail, color_head, sep_right)	
-			else:
-				wsp_items += "%s%%{F%s T1} %s " % (wsp_action, color_disable, wsp_name)
-		return '%s%s' % (wsp_icon, wsp_items)
+    def render_workspaces(self, display):
+        wsp_icon = "%%{F%s B%s} %%{T2}%s%%{T1}" % (color_back, color_head, icon_wsp)
+        wsp_items = ''
+        for wsp in self.i3.get_workspaces():
+            wsp_name = wsp['name']
+            wsp_action = ""
+            if wsp['output'] != display and not wsp['urgent']:
+                continue
+            if wsp['focused']:
+                # Possibly add dark red tint if urgent
+                wsp_items += "%%{F%s B%s}%s%s%%{F%s B%s T1} %s %%{F%s B%s}%s" % (color_head,
+                                                                                 color_wsp, sep_right, wsp_action,
+                                                                                 color_back, color_wsp, wsp_name,
+                                                                                 color_wsp, color_head, sep_right)
+            elif wsp['urgent']:
+                wsp_items += "%%{F%s B%s}%s%s%%{F%s B%s T1} %s %%{F%s B%s}%s" % (color_head,
+                                                                                 color_mail, sep_right, wsp_action,
+                                                                                 color_back, color_mail, wsp_name,
+                                                                                 color_mail, color_head, sep_right)
+            else:
+                wsp_items += "%s%%{F%s T1} %s " % (wsp_action, color_disable, wsp_name)
+        return '%s%s' % (wsp_icon, wsp_items)
 
-	def render_focused_title(self):
-		return "%%{F%s B%s}%s%%{F%s B%s T2} %s %%{F%s B-}%s%%{F- B- T1} %s" % (color_head,
-			color_sec_b2, sep_right, color_head, color_sec_b2, icon_prog, color_sec_b2,
-			sep_right, self.focusedWinTitle)
+    def render_focused_title(self):
+        return "%%{F%s B%s}%s%%{F%s B%s T2} %s %%{F%s B-}%s%%{F- B- T1} %s" % (color_head,
+                                                                               color_sec_b2, sep_right, color_head,
+                                                                               color_sec_b2, icon_prog, color_sec_b2,
+                                                                               sep_right, self.focusedWinTitle)
 
-	def render_music(self):
-		song = "fail"
-		try:
-			p = Popen(["/home/jeff/bin/media_dispatch.py", "current"], stdout=PIPE)
-			try:
-				song = p.communicate(timeout=5)[0].decode('utf-8').strip()
-			except TimeoutExpired:
-				p.kill()
-		except:
-			pass
-		cmusik = "%%{F%s }%s%%{F%s B%s} %%{T2}%s%%{F- T1} %s" % (color_sec_b1, sep_left,
-        	color_icon, color_sec_b1, icon_music, song)
-		return cmusik
+    def render_music(self):
+        song = "fail"
+        try:
+            p = Popen(["/home/jeff/bin/media_dispatch.py", "current"], stdout=PIPE)
+            try:
+                song = p.communicate(timeout=5)[0].decode('utf-8').strip()
+            except TimeoutExpired:
+                p.kill()
+        except:
+            pass
+        cmusik = "%%{F%s }%s%%{F%s B%s} %%{T2}%s%%{F- T1} %s " % (color_sec_b1, sep_left,
+                                                                 color_icon, color_sec_b1, icon_music, song)
+        return cmusik
 
-	def render_datetime(self):
-		cdate = "%%{F%s}%s%%{F%s B%s} %%{T2}%s%%{F- T1} %s" % (color_sec_b1, sep_left,
-			color_icon, color_sec_b1, icon_clock, time.strftime("%d.%m.%Y"))
-		ctime = "%%{F%s}%s%%{F%s B%s} %s %%{F- B-}" % (color_head, sep_left, color_back,
-			color_head, time.strftime("%H.%M.%S"))
-		return "%s%s%s" % (cdate, stab, ctime)
 
-	def render(self, caller=None, e=None):
-		# Render one bar per each output
-		out = ''
-		for idx,output in enumerate([ out.name for out in self.i3.get_outputs() if out['active'] ]):
-			out += "%%{S%d}%%{l}%s%s%%{r}%s%s" % (idx,
-				self.render_workspaces(display=output),
-				self.render_focused_title(),
-				self.render_music(),
-				self.render_datetime()
-			)
-		print(out)
-		sys.stdout.flush()
+    def render_loadavg(self):
+        try:
+            with open('/proc/loadavg', 'r') as loadfile:
+                avg = loadfile.read().split()[0]
+        except:
+            pass
+        color = color_fore if 5.6 > float(avg) else color_mail
+        cavg = "%%{F%s }%s%%{F- B%s} %%{T2}%s%%{F%s T1} %s" % (color_sec_b1, sep_left, color_sec_b1, icon_cpu, color, avg)
+        return cavg
+
+    def render_datetime(self):
+        cdate = "%%{F%s}%s%%{F%s B%s} %%{T2}%s%%{T1} %s" % (color_wsp, sep_left,
+                                            color_back, color_wsp, icon_clock,
+                                                            time.strftime("%d.%m.%Y"))
+
+        ctime = "%%{F%s}%s%%{F%s B%s} %s %%{F- B-}" % (color_head, sep_left, color_back,
+                                                       color_head, time.strftime("%H.%M.%S"))
+        return "%s%s%s" % (cdate, stab, ctime)
+
+    def render(self, caller=None, e=None):
+        # Render one bar per each output
+        out = ''
+        for idx, output in enumerate([out.name for out in self.i3.get_outputs() if out['active']]):
+            out += "%%{S%d}%%{l}%s%s%%{r}%s%s%s" % (idx,
+                                                  self.render_workspaces(display=output),
+                                                  self.render_focused_title(),
+                                                  self.render_loadavg(),
+                                                  self.render_music(),
+                                                  self.render_datetime()
+                                                  )
+        print(out)
+        sys.stdout.flush()
+
 
 def shutdown(caller):
-	lemonpid=int(check_output('pidof -s lemonbar', shell=True))
-	if lemonpid:
-		os.kill(lemonpid, SIGTERM)
-	sys.exit(0)
+    lemonpid = int(check_output('pidof -s lemonbar', shell=True))
+    if lemonpid:
+        os.kill(lemonpid, SIGTERM)
+    sys.exit(0)
+
 
 def run():
-	i3 = i3ipc.Connection()
-	i3thread = Thread(target=i3.main)
-	lemonbar = LemonBar(i3)
-	lemonbar.render()
+    i3 = i3ipc.Connection()
+    i3thread = Thread(target=i3.main)
+    lemonbar = LemonBar(i3)
+    lemonbar.render()
 
-	i3.on('workspace::focus', lemonbar.render)
-	i3.on('window::title',    lemonbar.on_window_title_change)
-	i3.on('window::focus',    lemonbar.on_window_title_change)
-	i3.on('window::urgent',   lemonbar.render)
-	i3.on('ipc-shutdown',     shutdown)
+    i3.on('workspace::focus', lemonbar.render)
+    i3.on('window::title', lemonbar.on_window_title_change)
+    i3.on('window::focus', lemonbar.on_window_title_change)
+    i3.on('window::urgent', lemonbar.render)
+    i3.on('ipc-shutdown', shutdown)
 
-	i3thread.start()
+    i3thread.start()
